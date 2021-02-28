a8a1608d5324ed559069e4f57a4563adf41026cd
sudo-update <seanjaviya@csu.fullerton.edu>
2021-02-28 12:52:24 -0800

    Ready for submission

--- Quadratic.asm
+++ Quadratic.asm
@@ -6,17 +6,19 @@
 ;One root is returned.  If there are no real roots, 0 is returned.
 ;Copyright (C) 2021 Sean Javiya.                                                                           *
 ;
-; This file is part of the software QuadraticFormula                                                                   *
-; QuadraticFormula is free software: you can redistribute it and/or modify it
-; under the terms of the GNU Lesser General Public License version 3 as
-; published by the Free Software Foundation.  This program is distributed in
-; the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-; implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-; See the GNU Lesser General Public License for more details.  A copy of the
-; GNU Lesser General Public License 3.0 should have been distributed with this
-; function.  If the LGPL does not accompany this software then it is available
-; here:
-; <https::www.gnu.org/licenses/>.
+; This file is part of the software QuadraticFormula
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ; *****************************************************************************
 ;
 ;
